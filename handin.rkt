994b2a3a1373be93602e5474b19459bac8f2e83f
Nathan Lilienthal <nathan@nixpulvis.com>
2014-09-24 13:18:24 -0400

    Starting to really make this.

--- handin.rkt
+++ handin.rkt
@@ -1,6 +1,10 @@
-#lang racket/base
+#lang racket
 
-(require racket/file racket/class racket/gui/base pl/client)
+(require racket/file racket/class pl/client)
+(require racket/gui/base)  ; TODO: Remove need for this.
+
+;; Submission Structure.
+(define-struct submission (username password filename))
 
 ;; This is a command line interface for the PL handin server. To use this
 ;; program you MUST have the pl package installed in your distribution of
@@ -10,49 +14,80 @@
 ;; connection : #<handin>
 (define connection (handin-connect "pl.barzilay.org" 9770))
 
-;; Retrieve the active assignments available for hand in, and bind them
-;; to `available-assignments`.
-;; available-assignments : [List-of String]
-(define available-assignments (retrieve-active-assignments connection))
+
+;; Command Line Parsing
+;; --------------------
+
+; A command is one of:
+;; - list
+;; - submit
+;; commands : [Listof String]
+(define commands '("list" "submit"))
+
+;; Retrive the command for the program.
+;; command : String
+(define command (vector-ref (current-command-line-arguments) 0))
+
+;; Validate `command`.
+(unless (ormap (lambda (c) (string=? command c)) commands)
+  (error "bad command" command))
 
 
-;;; PLAYGROUND
-(define password "<no>")
+;; Commands
+;; --------
 
-;; Get the input file.
+(define (list)
+  ;; Retrieve the active assignments available for hand in, and bind them
+  ;; to `available-assignments`.
+  ;; available-assignments : [List-of String]
+  (define available-assignments (retrieve-active-assignments connection))
 
-; TODO: Better parsing.
-(define argv (current-command-line-arguments))
-(define argc (vector-length argv))
+  ;; Display the available assignments.
+  (displayln available-assignments))
 
-(unless (= argc 1) (error "No file given"))
+(define (submit)
+  ;; Retrieve the submission structure from the command line.
+  ;; user-submission : #<submission>
+  (define user-submission (command-line
+                            #:program "handin submit"
+                            #:argv (vector-drop (current-command-line-arguments) 1)
+                            #:args (username password filename)
+                            (make-submission username password filename)))
 
-(define text (new text%))
-(send text insert (file->string (vector-ref argv 0)))
-(send text save-file ".out" 'same #f)
+  ;; Make a text GUI element.
+  (define text (new text%))
+  (send text insert (file->string (submission-filename user-submission)))
+  (send text save-file ".out" 'same #f)
 
-;; Print the available assignments.
-available-assignments
+  ;; Submit a file to scratch assignment.
+  (submit-assignment connection
+                     (submission-username user-submission)
+                     (submission-password user-submission)
+                     "hw03"
+                     (file->bytes ".out")
+                     (lambda () (printf "Committing..."))
+                     (lambda (m) (printf "~a~n" m))
+                     (lambda (m) (printf "!! ~a~n" m) #t)
+                     (lambda (msg styles) 'yes)))
 
-;; Submit a file to scratch assignment.
-(submit-assignment connection
-                   "nixpulvis"
-                   password
-                   "hw02"
-                   (file->bytes ".out")
-                   (lambda () (printf "Committing..."))
-                   (lambda (m) (printf "~a~n" m))
-                   (lambda (m) (printf "!! ~a~n" m) #t)
-                   (lambda (msg styles) 'yes))
 
-;; Reform the connection.
-(set! connection (handin-connect "pl.barzilay.org" 9770))
+;; Command Running
+;; ---------------
 
-;; Get the handin server's copy of the submission for scratch assignment.
-(define file (retrieve-assignment connection "nixpulvis" password "hw02"))
+(cond [(string=? command "list") (list)]
+      [(string=? command "submit") (submit)])
 
-;; Write the file.
-(define out (open-output-file ".in"))
-(write-bytes file out)
 
-;;;
+; ;;; PLAYGROUND
+
+; ;; Reform the connection.
+; (set! connection (handin-connect "pl.barzilay.org" 9770))
+
+; ;; Get the handin server's copy of the submission for scratch assignment.
+; (define file (retrieve-assignment connection "nixpulvis" password "hw02"))
+
+; ;; Write the file.
+; (define out (open-output-file ".in"))
+; (write-bytes file out)
+
+; ;;;
